b0647fe89c56ac35497ee722eae60ba7a0111ce0
Spindeel <112945787+Spindeel@users.noreply.github.com>
2022-11-30 14:15:46 +0100

    Add files via upload

---
+++ shop.js
@@ -0,0 +1,51 @@
+function Vip1() {
+    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+}
+
+function Vip2() {
+    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+}
+
+function Vip3() {
+    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+}
+
+function Vip4() {
+    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+}
+
+function Vip5() {
+    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+}
+
+function Vip6() {
+    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+}
+
+function Vip7() {
+    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+}
+
+function Vip8() {
+    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+}
+
+function Vip9() {
+    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+}
+
+function Vip10() {
+    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+}
+
+function Vip11() {
+    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+}
+
+function Vip12() {
+    window.location.href = "https://github.com/Tiagox42/Minecraft-WebSite";
+}
+
+function Tak() {
+    window.location.href = "https://tipply.pl/u/spindeell";
+}
